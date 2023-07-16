67e9f5b654d128aeb80690dcd965243372e418a3
Tibor <tibor736@hotmail.com>
2023-07-16 23:10:27 +0300

    added Items and singleItem component. Add to list and delete from list items

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
 import Form from "./Form";
+import Items from "./Items";
+import { nanoid } from "nanoid";
 
 const App = () => {
-	const [items, setItems] = useState();
+	const [items, setItems] = useState([]);
+
+	const addItem = (itemName) => {
+		const newItem = {
+			name: itemName,
+			completed: false,
+			id: nanoid(),
+		};
+		setItems([...items, newItem]);
+	};
+
+	const removeItem = (itemId) => {
+		const newItems = items.filter((item) => item.id !== itemId);
+		setItems(newItems);
+	};
+
 	return (
 		<section className="section-center">
-			<Form />
+			<Form addItem={addItem} />
+			<Items items={items} removeItem={removeItem} />
 		</section>
 	);
 };
